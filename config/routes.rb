1863828cb70e53dab1ee694d1d9482b1bbdaaf3b
tboccuto <59341700+tboccuto@users.noreply.github.com>
2022-05-25 22:32:20 -0400

    updating navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   #get 'home/index'
   #each page need controller ie in terminal type [rails g controller home index]
   #to see all routes run [rails routes]
-  get 'home/about'
   root 'home#index'
+  get 'home/about'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
