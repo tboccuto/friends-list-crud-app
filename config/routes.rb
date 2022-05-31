cf8d80a597d56ea6482536389b52fc3346dd5304
tboccuto <59341700+tboccuto@users.noreply.github.com>
2022-05-30 22:14:46 -0400

    fixing devise routes for sign in & sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  devise_for :users do
+  get '/users/sign_out' => 'devise/sessions#destroy' #hint look at rails routes, out of box does not work!
+  get'users/sign_in' => 'devise/sessions#new' #hint look at rails routes
+
+end
+
+  #devise_for :users, controllers: { registrations: "registrations" }
   resources :friends
   #get 'home/index'
   #each page need controller ie in terminal type [rails g controller home index]
