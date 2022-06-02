e703fa8b2beff94dc6e5cd0a3a1839eadc228be3
tboccuto <59341700+tboccuto@users.noreply.github.com>
2022-06-01 21:23:37 -0400

    fixing up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,12 @@
 Rails.application.routes.draw do
-  
-  #devise_for :users  do
-  #get '/users/sign_out' => 'devise/sessions#destroy' #hint look at rails routes, out of box does not work!
-  #get'users/sign_in' => 'devise/sessions#new' #hint look at rails routes
-
   devise_for :users, controllers: { registrations: "registrations" }
-
-#end
-
-  #devise_for :users, controllers: { registrations: "registrations" }
   resources :friends
   #get 'home/index'
   #each page need controller ie in terminal type [rails g controller home index]
   #to see all routes run [rails routes]
   root 'home#index'
   get 'home/about'
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
