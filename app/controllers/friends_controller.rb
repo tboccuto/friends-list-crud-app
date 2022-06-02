9a28b50a7bc5c24264ab0d3e3d141bcecadc8d2f
tboccuto <59341700+tboccuto@users.noreply.github.com>
2022-06-01 21:38:30 -0400

    adding user_id param to friends_controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -65,6 +65,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twitter, :instagram)
+      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twitter, :instagram, :user_id)
     end
 end
